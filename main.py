b22f4090b06d9ef0efab38555353bc8fda29bbb8
melisaterlemis <83540629+melisaterlemis@users.noreply.github.com>
2024-05-23 15:28:32 +0300

    time sleep eklenmesi

--- main.py
+++ main.py
@@ -13,35 +13,41 @@ email_input=browser.find_element(By.XPATH,'//*[@id="ap_email"]')
 try:
  email_input.send_keys('devvmelisaa@gmail.com')
  email_input.send_keys(Keys.RETURN)
+ time.sleep(5)
 
  password_input=browser.find_element(By.XPATH,'//*[@id="ap_password"]')
  password_input.send_keys('Melisa1234.')
  password_input.send_keys(Keys.RETURN)
+ time.sleep(5)
 
- print('Başarılı giriş.')
+ print('Test1: Başarılı giriş.')
 except:
- print('Başarısız giriş.')
+ print('Test1: Başarısız giriş.')
 
 try:
  search_input=browser.find_element(By.XPATH,'//*[@id="twotabsearchtextbox"]')
  search_input.send_keys('kitap')
  search_input.send_keys(Keys.RETURN)
- print('Arama basarili.')
+ time.sleep(5)
+ print('Test2: Arama basarili.')
 except:
- print("Arama basarisiz.")
+ print("Test2: Arama basarisiz.")
  
 try:
  kizil_veba=browser.find_element(By.XPATH, '//*[@id="search"]/div[1]/div[1]/div/span[1]/div[1]/div[7]/div/div/span/div/div/div[3]/div[1]/h2/a/span').click()
- print('Kitap seçildi.')
+ time.sleep(5)
+ print('Test3: Kitap seçildi.')
 except:
-  print('Kitap seçilmedi.')
+  print('Test3: Kitap seçilmedi.')
 
 try:
  sepete_ekle_button=browser.find_element(By.XPATH, '//*[@id="add-to-cart-button"]').click()
+ time.sleep(5)
  sepet_button=browser.find_element(By.XPATH, '//*[@id="nav-cart-text-container"]').click()
- print('Sepete ekleme basarili.')
+ time.sleep(5)
+ print('Test4: Sepete ekleme basarili.')
 except:
- print('Sepete ekleme basarisiz.')
+ print('Test4: Sepete ekleme basarisiz.')
 
 time.sleep(100)
 #browser.quit()
