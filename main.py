5ab051caedd48dc45251f6715052af93218a2bf5
melisaterlemis <83540629+melisaterlemis@users.noreply.github.com>
2024-05-23 15:13:52 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+browser = webdriver.Chrome()
+browser.get('https://www.amazon.com.tr/')
+browser.maximize_window()
+cookie_button=browser.find_element(By.XPATH,'//*[@id="sp-cc-accept"]').click()
+login_button=browser.find_element(By.XPATH,'//*[@id="nav-link-accountList"]/span').click()
+email_input=browser.find_element(By.XPATH,'//*[@id="ap_email"]')
+try:
+ email_input.send_keys('devvmelisaa@gmail.com')
+ email_input.send_keys(Keys.RETURN)
+
+ password_input=browser.find_element(By.XPATH,'//*[@id="ap_password"]')
+ password_input.send_keys('Melisa1234.')
+ password_input.send_keys(Keys.RETURN)
+
+ print('Başarılı giriş.')
+except:
+ print('Başarısız giriş.')
+
+try:
+ search_input=browser.find_element(By.XPATH,'//*[@id="twotabsearchtextbox"]')
+ search_input.send_keys('kitap')
+ search_input.send_keys(Keys.RETURN)
+ print('Arama basarili.')
+except:
+ print("Arama basarisiz.")
+ 
+try:
+ kizil_veba=browser.find_element(By.XPATH, '//*[@id="search"]/div[1]/div[1]/div/span[1]/div[1]/div[7]/div/div/span/div/div/div[3]/div[1]/h2/a/span').click()
+ print('Kitap seçildi.')
+except:
+  print('Kitap seçilmedi.')
+
+try:
+ sepete_ekle_button=browser.find_element(By.XPATH, '//*[@id="add-to-cart-button"]').click()
+ sepet_button=browser.find_element(By.XPATH, '//*[@id="nav-cart-text-container"]').click()
+ print('Sepete ekleme basarili.')
+except:
+ print('Sepete ekleme basarisiz.')
+
+time.sleep(100)
+#browser.quit()
+
+ 
